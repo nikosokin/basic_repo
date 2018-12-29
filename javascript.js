e80987851469b168ca9471814a4806fe690bb19f
nikosokin <niko.osokin@gmail.com>
2018-12-29 00:53:40 -0800

    modified js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,3 @@
-document.getElementById("js ").innerHTML ="<b>This is a test</b>";
+document.getElementById("js").innerHTML ="<b>This is a test</b>";
+
+document.getElementById("container").innerText = "I have replaced the original text";
